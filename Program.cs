437cda1d0a5a3a5f26c8f34b2ab4b6fc90e96901
Snaider Ssj <snaiderquispe603@gmail.com>
2025-11-25 09:51:53 -0400

    Refactor database connection and migration setup
    Updated database connection handling for Railway and local development. Added automatic migrations on startup.

--- Program.cs
+++ Program.cs
@@ -1,40 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using MarketingLaPazAPI.Infraestructura.Data;
-using MarketingLaPazAPI.Infraestructura.Repositorios;
-using MarketingLaPazAPI.Core.Interfaces;
-using MarketingLaPazAPI.Core.Servicios;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Obtener la cadena de conexion de Railway
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+Console.WriteLine($"La cadena de conexion es esta: {connectionString}");
+
 // Add services to the container
 builder.Services.AddControllers();
+
+// Configurar PostgreSQL
+if (!string.IsNullOrEmpty(connectionString))
+{
+    // Parsear DATABASE_URL de Railway
+    var parsedConnectionString = ParseDatabaseUrl(connectionString);
+    builder.Services.AddDbContext<MarketingDbContext>(options =>
+        options.UseNpgsql(parsedConnectionString));
+}
+else
+{
+    // Para desarrollo local
+    builder.Services.AddDbContext<MarketingDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("MarketingDB")));
+}
+
+// Learn more about configuring Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Entity Framework con PostgreSQL
-builder.Services.AddDbContext<MarketingDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("MarketingDB")));
-
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("MarketingPolicy", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000", "http://127.0.0.1:5500")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
-
-// Repositorios
-builder.Services.AddScoped<ICampañaRepositorio, CampañaRepositorio>();
-builder.Services.AddScoped<ILeadRepositorio, LeadRepositorio>();
-
-// Servicios
-builder.Services.AddScoped<IServicioCampaña, ServicioCampaña>();
+// Configurar el puerto para Railway
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 var app = builder.Build();
+
+// Ejecutar migraciones automaticamente al iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<MarketingDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
@@ -44,8 +49,19 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors("MarketingPolicy");
 app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Metodo para convertir DATABASE_URL de Railway a connection string
+static string ParseDatabaseUrl(string databaseUrl)
+{
+    if (string.IsNullOrEmpty(databaseUrl))
+        return "Host=localhost;Database=MarketingLaPazDB;Username=postgres;Password=password";
+    
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    
+    return $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true;";
+}
